45a2d999a2ab30cd247acee42a96de60a0029594
Sumit Pandey <sumit_pandey@outlook.com>
2019-08-06 00:38:58 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,3 @@
+const userName = localStorage.getItem('userName');
+const name = document.getElementById('name');
+name.textContent = userName;
---
+++ signup.js
@@ -0,0 +1,6 @@
+// let inputUser = 'username';
+// // let email = 'email';
+// let password = 'passcode';
+// const userName = document.getElementById('username');
+// const email = document.getElementById('email');
+// const password = document.getElementById('passcode');
